567c5aa3cd2c35127bd5127a0e7028115cf39bd0
Toby Clarke <teeohbee@gmail.com>
2015-11-18 11:15:51 +0000

    connects to local mongodb

--- server.js
+++ server.js
@@ -2,15 +2,53 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Connection Error : '));
+db.once('open', function(){
+  console.log('Connection ok!');
+});
+
+var Bear = require('./app/models/bear');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
 var router = express.Router();
 
+router.use(function(req, res, next) {
+  console.log('Something is happening');
+  next();
+});
+
 router.get('/', function(req, res) {
-  res.json({ message: 'hooray! welcome to our api!' });
+  res.json({
+    message: 'hooray! welcome to our api!'
+  });
+});
+
+router.route('/bears')
+
+.post(function(req, res) {
+  var bear = new Bear();
+  bear.name = req.body.name;
+
+  bear.save(function(err) {
+    if (err)
+      res.send(err);
+
+    res.json({
+      message: "Bear created!"
+    });
+  });
+
 });
 
 app.use('/api', router);
